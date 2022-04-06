f31dbe5ce6cc4d11a91283747b9d3fb58b49c378
fernando-dev21 <fernando.dev21@outlook.com>
2022-04-06 11:35:56 -0300

    https://github.com/fernando-dev21/Cadastro_Cliente.git

---
+++ db.js
@@ -0,0 +1,41 @@
+async function connect(){
+    //confirma se está conectado com a variavel global
+    if(global.connection && global.connection.state !== 'disconnected'){
+        return global.connection;
+    }    
+    
+    const mysql = require("mysql/promisse");
+    const connection = await mysql.createConnection("mysql://root:alunofatec@localhost:3306/webii");
+    console.log("Conectou no MySQL!");
+    global.connection = connection;
+    return connection; 
+}   
+
+    async function selectUsuario(){
+        const conn = await connect();
+        const [rows] = await conn.query('SELECT * FROM usuario;');
+        return rows;
+    }
+
+    async function insetUsuario(usuario){
+        const conn = await connect();
+        const sql = 'INSERT INTO usuario(nome,senha) VALUES (?,?);';
+        const values = [usuario.nome, usuario.senha];
+        return await conn.query(sql, values);
+    }
+
+    async function deleteUsuario(id){
+        const conn = await connect();
+        const sql = 'DELETE FROM usuario where id=?;';
+        return await conn.query(sql, [id]);
+    }
+
+    async function updateUsuario(id, usuario){
+        const conn = await connect();
+        const sql = 'UPDATE usuario SET nome=?, senha=? WHERE id=?';
+        const values = [usuario.nome, usuario.senha, id];
+        return await conn.query(sql, values);
+    }
+
+ 
+module.exports = {selectUsuario, insetUsuario, deleteUsuario, updateUsuario};
---
+++ index.js
@@ -0,0 +1,19 @@
+(async() => {
+ 
+const db = require(".\db");
+console.log('SELECT * FROM USUARIO');
+const usuarios = await db.selectUsuario();
+console.log(usuarios);
+
+console.log('INSERT INTO USUARIO');
+const result = await db.insertUsuario({nome: "Zé", senha: "shiduhsaud332"});
+console.log(result);
+
+console.log('DELETE FROM usuario');
+const result3 = await db.deleteUsuario(2);
+console.log(result3);
+
+console.log('UPDATE USUARIO');
+const result2 = await db.updateUsuario(3, {nome: "Zé José", senha: "ahsuahsuisdbvc7421"});
+console.log(result2);
+})();
